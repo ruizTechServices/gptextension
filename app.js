8a444b47117c36b754d52180a7c512d6211fce9a
Giovanni <ruiztechservices@gmail.com>
2024-10-06 12:16:08 -0400

    modified routes pt4

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//app.js
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const chatRoutes = require('./chatRoutes');
 const modelRoutes = require('./modelRoutes');
 
@@ -9,15 +9,20 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', chatRoutes);
 app.use('/api', modelRoutes);
+
+app.get('/', (req, res) => {
+  res.send('24HourGPT Backend is running!');
+});
 
 app.use((req, res) => {
   res.status(404).send('404 Not Found');
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on port ${port}`);
 });
